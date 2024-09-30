9002ca80b39ba6312333a5178a89a83ac1f6697c
joeprathap <joeprathap.pj@sudoboat.com>
2024-09-30 17:54:13 +0530

    port

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-server.listen(5000, () => {
+server.listen(10000, () => {
   console.log("Server is running on port 5000");
 });
 
